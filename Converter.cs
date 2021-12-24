0122506e06e95f75274406a0fe2b4e928462bdef
evgeny-belyaev <evgeny.belyaev@gmail.com>
2021-12-24 20:55:54 +0300

    helix controller initial commit

---
+++ Converter.cs
@@ -0,0 +1,76 @@
+﻿using NAudio.Midi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelixController
+{
+    class Converter
+    {
+        private MidiIn midiIn;
+        private MidiOut midiOut;
+
+        private int currentPreset = 0;
+        private byte[] snapshot1 = new byte[] { 0xBC, 0x45, 0x00 };
+        private byte[] snapshot2 = new byte[] { 0xBC, 0x45, 0x01 };
+        private byte[] snapshot3 = new byte[] { 0xBC, 0x45, 0x02 };
+        private byte[] snapshot4 = new byte[] { 0xBC, 0x45, 0x03 };
+
+
+        public Converter(MidiIn midiIn, MidiOut midiOut)
+        {
+            this.midiIn = midiIn;
+            this.midiOut = midiOut;
+        }
+        public void start()
+        {
+            midiIn.MessageReceived += midiIn_MessageReceived;
+            midiIn.Start();
+        }
+
+        void midiIn_MessageReceived(object sender, MidiInMessageEventArgs e)
+        {
+            var number = (e.RawMessage - 0xC0) / 256;
+            var snapshotNumber = number % 4;
+            byte presetNumber = (byte)(number / 4);
+            var presetChanged = presetNumber != currentPreset;
+
+            if (presetChanged)
+            {
+                currentPreset = presetNumber;
+                midiOut.SendBuffer(new byte[] { 0xCC, presetNumber });
+                Thread.Sleep(50);
+            }
+
+            switch (snapshotNumber)
+            {
+                case 0:
+                    {
+                        midiOut.SendBuffer(snapshot1);
+                        break;
+                    }
+                case 1:
+                    {
+                        midiOut.SendBuffer(snapshot2);
+                        break;
+                    }
+                case 2:
+                    {
+                        midiOut.SendBuffer(snapshot3);
+                        break;
+                    }
+                case 3:
+                    {
+                        midiOut.SendBuffer(snapshot4);
+                        break;
+                    }
+
+            }
+
+            Console.WriteLine($"Preset: {presetNumber}, Snapshot: {snapshotNumber}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using NAudio.Midi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelixController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputDevideName = "TSMIDI2.0";
+            var outputDeviceName = "loopMIDI";
+
+            int outputDeviceId = GetOutDevice(outputDeviceName);
+            if (outputDeviceId == -1)
+            {
+                Console.WriteLine(outputDeviceName + " not found!");
+                return;
+            }
+            var midiOut = new MidiOut(outputDeviceId);
+            Console.WriteLine(outputDeviceName + " connected successfully");
+
+
+            int inputDeviceId = GetInDevice(inputDevideName);
+            if (inputDeviceId == -1)
+            {
+                Console.WriteLine(inputDevideName + " not found!");
+                return;
+            }
+            var midiIn = new MidiIn(inputDeviceId);
+
+            Console.WriteLine(inputDevideName + " connected successfully");
+
+            var converter = new Converter(midiIn, midiOut);
+            converter.start();
+            
+            Console.ReadKey();
+        }
+
+
+        static int GetOutDevice(string deviceName)
+        {
+            for (int device = 0; device < MidiOut.NumberOfDevices; device++)
+            {
+                var deviceInfo = MidiOut.DeviceInfo(device);
+                var productName = deviceInfo.ProductName;
+
+                if (productName.IndexOf(deviceName, StringComparison.Ordinal) != -1)
+                {
+                    return device;
+                }
+            }
+
+            return -1;
+        }
+
+        static int GetInDevice(string deviceName)
+        {
+            for (int device = 0; device < MidiIn.NumberOfDevices; device++)
+            {
+                var deviceInfo = MidiIn.DeviceInfo(device);
+                var productName = deviceInfo.ProductName;
+
+                if (productName.IndexOf(deviceName, StringComparison.Ordinal) != -1)
+                {
+                    return device;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
